1e6182d97d4cd41993da693c9d7c7c8e9b8dfbda
Iuliia Ivanova <kapitanavokado12@gmail.com>
2022-11-27 07:31:09 +0300

    Updated UI Kit

--- src/components/ImageItem/view.tsx
+++ src/components/ImageItem/view.tsx
@@ -7,8 +7,6 @@ import { H4 } from "components/UIKit/text";
 
 import { PixabayImage } from "types/pixabay";
 
-import { Colors } from "constants/colors";
-
 interface Props {
   data: PixabayImage;
   onPress: () => void;
@@ -17,8 +15,8 @@ interface Props {
 const ImageItemView = ({ data, onPress }: Props) => {
   return (
     <TouchableOpacity onPress={onPress} style={styles.container}>
-      <Image source={{ uri: data.previewURL }} style={styles.image} />
-      <H4 color={Colors.gray60}>{`${data.likes} likes`}</H4>
+      <Image source={{ uri: data.webformatURL }} style={styles.image} />
+      <H4>{`${data.imageHeight}x${data.imageWidth}`}</H4>
     </TouchableOpacity>
   );
 };
--- src/components/SearchField/container.tsx
+++ src/components/SearchField/container.tsx
@@ -3,23 +3,30 @@ import React, { useState } from "react";
 import SearchFieldView from "./view";
 
 interface Props {
+  value: string;
   onChange: (value: string) => void;
+  onClear: () => void;
 }
 
 // TODO: add x to clear
 // TODO: expand modal input to allow multiple lines
 
-const SearchFieldContainer = ({ onChange }: Props) => {
-  const [value, setValue] = useState("");
+const SearchFieldContainer = ({
+  onChange,
+  onClear,
+  value: initialValue,
+}: Props) => {
+  const [value, setValue] = useState(initialValue);
   const [isFocused, setFocused] = useState(false);
 
   const handleChange = (val: string) => {
     setValue(val);
-    onChange && onChange(val);
+    onChange(val);
   };
 
   const handleClear = () => {
     setValue("");
+    onClear();
   };
 
   // ref not working for styling
